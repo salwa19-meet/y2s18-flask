84c17912229e992d799293c5dc2fc9e3757f7eef
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:12:21 +0300

    finish

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    pass
+	sports = ["football" , "golf" , "basktboll"] 
+	return render_template("index.html", sports=sports , like_same_sports=False)
+
+    
 
 if __name__ == '__main__':
    app.run(debug = True)
